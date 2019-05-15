681ae3bf5e807ba2ad0a82e711052d8b379871da
Lennert De Ryck <admin@lennertderyck.be>
2019-05-15 10:38:58 +0000

    Deploy from Forestry.io

---
+++ assets/js/menu.js
@@ -0,0 +1,7 @@
+var menuCollapse = document.getElementById("NavMainCollapse");
+var btnNavMainCollapse = document.getElementById("btnNavMainCollapse");
+var body = document.getElementById("body");
+
+function checkMenuOpen() {
+    body.classList.toggle("no-scroll");
+}
