4fcae6ad12bdf3bea52d2b6ac745e40cb2328c67
gdm-lenndery <32176355+gdm-1718-lenndery@users.noreply.github.com>
2019-06-13 23:31:42 +0200

    Update

--- assets/js/humanCheck.js
+++ assets/js/humanCheck.js
@@ -19,11 +19,11 @@ function areYouHumanCheck() {
         formContact.setAttribute('action', ' ');
         notifHumanCheck.style.display = "inline-block";
         btnFormSubmit.classList.add('disabled');
-        createCookie('hmn', 'false', 1000);
+        createCookie('hmn', 'false', 7);
     }
 }
 
-if (humanCheckCookie = "false") {
+if (humanCheckCookie == "false") {
     humanCheck = document.getElementById('humanCheck');
     btnFormSubmit = document.getElementById('btnFormSubmit');
     notifHumanCheck = document.getElementById('notifHumanCheck');
