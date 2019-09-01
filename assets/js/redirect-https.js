0c6d56b407657163d257c3a15936dc7cd41d3612
gdm-lenndery <32176355+gdm-1718-lenndery@users.noreply.github.com>
2019-09-01 19:38:25 +0200

    Update

--- assets/js/redirect-https.js
+++ assets/js/redirect-https.js
@@ -11,7 +11,9 @@ if (window.location.href.startsWith("http://lennertderyck.be")) {
 console.log('    ' + 'location is ' + window.location.href);
 
 if (window.location.href.startsWith("http://127.0.0.1") == false) {
-    if (window.location.href.startsWith("http://lennertderyck.be") == false) {
+    if (window.location.href.startsWith("http://lennertderyck.be")) {
+        console.log('    ' + 'URL begins with https://lennertderyck.be');
+    } else {
         console.log('    ' + 'URL does not start with https://lennertderyck.be');
         var html = document.getElementById('html');
         html.classList.add('embedded');
